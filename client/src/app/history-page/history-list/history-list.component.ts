a08e9619a3eaf86a53f6695da5d96d19844b8f2a
tabachkov <tabachkovav@gmail.com>
2019-04-10 11:42:24 +0300

    display orders

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy
+} from '@angular/core';
 import { IOrder } from './../../shared/interfaces';
+import {
+  IMaterialInstance,
+  MaterialService
+} from 'src/app/shared/classes/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.css']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements AfterViewInit, OnDestroy {
   @Input() orders: IOrder[];
+  @ViewChild('modal') modalRef: ElementRef;
 
-  constructor() {}
+  modal: IMaterialInstance;
+  selectedOrder: IOrder;
 
-  ngOnInit() {}
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  computePrice(order: IOrder): number {
+    return order.list.reduce((total, item) => {
+      return (total += item.quantity * item.cost);
+    }, 0);
+  }
+
+  selectOrder(order: IOrder) {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  closeModal() {
+    this.modal.close();
+  }
 }
